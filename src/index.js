779f0aa60f1dce25037e586013c4ec9c64bca6c8
TofuWatagashi <takashi.oka2216@gmail.com>
2022-01-08 21:51:27 +0900

    const,let

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-import "./styles.css";
+/**
+ * const let の宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var";
+// console.log(val1);
+
+// //var変数は宇m￥和書き可能
+
+// val1 = "上書き";
+// console.log(val1);
+
+// var val1 = "再宣言";
+// console.log(val1);
+
+let val2 = "let";
+console.log(val2);
+
+val2 = "uwagaki";
+console.log(val2);
+
+const val3 = "uwagki dame";
+console.log(val3);
+
+const val4 = {
+  name: "nakagawa",
+  age: 33
+};
+
+console.log(val4);
+
+val4.name = "test";
+console.log(val4);
+
+const val5 = ["dog", "cat"];
+val5.push("monkey");
+console.log(val5);
